18a2c77683f3210bed106fae20ea0cdc18c515ca
KetaslavaKet <ketaslava@gmail.com>
2026-06-13 19:14:19 -0700

    Update gradle.properties, gradle-daemon-jvm.properties, and settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,9 +14,6 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-}
 
 dependencyResolutionManagement {
     repositories {
